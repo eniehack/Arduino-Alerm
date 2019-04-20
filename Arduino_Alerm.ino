5ae97eea35fbc023dad4f3556882ed11def3e78e
caffeine <eniehack@outlook.jp>
2019-04-20 16:36:08 +0900

    ESP32にて8bitのPWMができた

--- Arduino_Alerm.ino
+++ Arduino_Alerm.ino
@@ -1,21 +1,32 @@
-const int LED = 9;
-const int BUTTON = A0;
+const int LEDC_CHANNEL = 0;
+const int LED = 16;
+const int freq = 5000;
+const int resolution = 8;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  ledcSetup(LEDC_CHANNEL, freq, resolution);
+  ledcAttachPin(LED, LEDC_CHANNEL);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   for (int i = 0; i < 255; i++){
-    if (i < 100) {
-      analogWrite(LED, i);
-      delay(5000);
-    }
-    if (i >= 100) {
-      analogWrite(LED, i);
-      delay(100);
-    }
+      if (i < 100) {
+        Serial.println(i, DEC);
+        ledcWrite(LEDC_CHANNEL, i);
+        delay(5000);
+      }
+      if (i >= 100) {
+        Serial.println(i, DEC);
+        ledcWrite(LEDC_CHANNEL, i);
+        delay(100);
+      }
   }
 }
+/*
+void snooze() {
+  analogWrite(LED, 0);
+  // スヌーズ時間分delayを発生させる
+  delay(5000);
+}
+*/
