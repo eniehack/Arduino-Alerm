62eba20efbbf58ccdc9b66135845b133fc66454c
caffeine <eniehack@outlook.jp>
2019-04-29 15:39:08 +0900

    NTPサーバに接続し、時刻を取得できるようにした

--- Arduino_Alerm.ino
+++ Arduino_Alerm.ino
@@ -1,4 +1,8 @@
 #include "WiFi.h"
+#include "Wire.h"
+#include <RTClib.h>
+
+RTC_DS3231 rtc;
 
 const int LEDC_CHANNEL = 0;
 const int LED = 16;
@@ -6,7 +10,11 @@ const int freq = 5000;
 const int resolution = 8;
 
 void setup() {
+  Wire.begin(21,22);
+  delay(10);
+  
   Serial.begin(115200);
+  delay(1000);
 
   WiFi.mode(WIFI_AP_STA);
   WiFi.beginSmartConfig();
@@ -25,6 +33,9 @@ void setup() {
   }
 
   Serial.print("WiFi Connected");
+
+  configTime(9 * 3600L, 0, "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
+  delay(1000);
   
   ledcSetup(LEDC_CHANNEL, freq, resolution);
   ledcAttachPin(LED, LEDC_CHANNEL);
@@ -42,6 +53,14 @@ void loop() {
         ledcWrite(LEDC_CHANNEL, i);
         delay(100);
       }
+      struct tm timeInfo;
+      char s[20];
+       
+      getLocalTime(&timeInfo);
+      sprintf(s, " %04d/%02d/%02d %02d:%02d:%02d",
+              timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday,
+              timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
+      Serial.println(s);
   }
 }
 /*
